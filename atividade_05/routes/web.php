d55a8588fd0b63ce31a36d627b49c3349ce640aa
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-05-29 19:41:33 -0300

    configurando routes

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Auth;
+
+Route::resource('categories', CategoryController::class);
 
 Route::get('/', function () {
     return view('welcome');
