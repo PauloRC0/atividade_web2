0c86becd1c3ee0912841c55e7997c0421fc5485c
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-05-29 21:19:36 -0300

    ajustando create.blade de Author

--- atividade_05/resources/views/authors/create.blade.php
+++ atividade_05/resources/views/authors/create.blade.php
@@ -6,18 +6,22 @@
 
     <form action="{{ route('authors.store') }}" method="POST">
         @csrf
-
         <div class="mb-3">
-            <label for="name" class="form-label">Nome do Autor</label>
-            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}" required>
-
+            <label for="name" class="form-label">Nome</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" value="{{ old('name') }}" required>
             @error('name')
-                <div class="text-danger mt-1">{{ $message }}</div>
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
             @enderror
         </div>
 
-        <button type="submit" class="btn btn-success">Salvar</button>
-        <a href="{{ route('authors.index') }}" class="btn btn-secondary">Cancelar</a>
+        <button type="submit" class="btn btn-success">
+            <i class="bi bi-save"></i> Salvar
+        </button>
+        <a href="{{ route('authors.index') }}" class="btn btn-secondary">
+            <i class="bi bi-arrow-left"></i> Voltar
+        </a>
     </form>
 </div>
 @endsection
