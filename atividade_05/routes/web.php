63ea2c004f24295cfdcb03046f0f78cf7d7525d8
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-05-29 21:43:26 -0300

    adicionando publisher

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Auth;
 
 route::resource('authors', AuthorController::class);
+
+route::resource('publishers', PublisherController::class);
 
 Route::resource('categories', CategoryController::class);
 
