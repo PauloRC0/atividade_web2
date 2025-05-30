9094e202bff11765530fb87ea60abc1514329f4c
PauloRC0 <prcs3@discente.ifpe.edu.br>
2025-05-29 22:02:03 -0300

    criando BookController e ajustando rotas de Book

--- atividade_05/routes/web.php
+++ atividade_05/routes/web.php
@@ -5,6 +5,17 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PublisherController;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\BookController;
+
+// Rotas para criação de livros
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+// Rotas RESTful para index, show, edit, update, delete (tem que ficar depois das rotas /books/create-id-number e /books/create-select)
+Route::resource('books', BookController::class)->except(['create', 'store']);
 
 route::resource('authors', AuthorController::class);
 
